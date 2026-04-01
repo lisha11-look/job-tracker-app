04168be85c56b9410a6cb899dc27a7db7b73abfd
lisha11-look <akramrimsha6543@gmail.com>
2026-04-01 18:47:08 +0800

    Backend structure and test route setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 const PORT = 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const testRoutes = require("./routes/testRoutes");
